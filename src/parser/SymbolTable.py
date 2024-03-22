f86ec5d93808319291f5cadae14c5b83e98e8ea5
CedricDeVijt <cedric.devijt@gmail.com>
2024-03-22 14:53:43 +0100

    added new symbl table

--- src/parser/SymbolTable.py
+++ src/parser/SymbolTable.py
@@ -1,8 +1,9 @@
 class Symbol:
-    def __init__(self, name, varType, const, value=0):
+    def __init__(self, name, varType, const, value=0, returnType=None):
         self.name = name
         self.const = const
         self.type = varType
+        self.returnType = returnType
         self.value = value
 
 
@@ -21,34 +22,46 @@ class SymbolTable:
         symbol = self.symbols.get(name, None)
         if symbol:
             return symbol
-
         raise Exception(f"Symbol {name} not found in the table")
 
-    def __str__(self):
-        return "\n".join(
-            [f"{symbol.name}: {symbol.type} - Scope: {symbol.scope} - Accessibility: {symbol.accessibility}" for symbol
-             in self.symbols.values()])
+
+class TreeNode:
+    def __init__(self, data, parent=None):
+        self.data = data
+        self.parent = parent
+        self.children = []
 
 
 class SymbolTableBuilder:
     def __init__(self):
-        self.symbol_stack = [SymbolTable()]
+        self.root = TreeNode(SymbolTable())
+        self.current_node = self.root
 
     def open_scope(self):
-        self.symbol_stack.append(SymbolTable())
+        new_node = TreeNode(SymbolTable(), self.current_node)
+        self.current_node.children.append(new_node)
+        self.current_node = new_node
 
     def close_scope(self):
-        return self.symbol_stack.pop()
+        if self.current_node == self.root:
+            raise Exception("Cannot close root scope")
+        self.current_node = self.current_node.parent
 
     def add_symbol(self, symbol):
-        self.symbol_stack[-1].add_symbol(symbol)
+        self.current_node.data.add_symbol(symbol)
 
     def get_symbol(self, name):
-        for table in reversed(self.symbol_stack):
-            symbol = table.get_symbol(name)
+        return self._get_symbol_recursive(self.root, name)
+
+    def _get_symbol_recursive(self, node, name):
+        symbol = node.data.get_symbol(name)
+        if symbol:
+            return symbol
+        for child in node.children:
+            symbol = self._get_symbol_recursive(child, name)
             if symbol:
                 return symbol
         return None
 
     def current_scope(self):
-        return self.symbol_stack[-1]
+        return self.current_node.data
