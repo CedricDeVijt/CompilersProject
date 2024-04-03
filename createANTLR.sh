355c323b84b58bfff7d6183dcf125480521246ed
CedricDeVijt <cedric.devijt@gmail.com>
2024-04-03 14:22:14 +0200

    actual final fix

--- createANTLR.sh
+++ createANTLR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 java -jar antlr-4.13.1-complete.jar -Dlanguage=Python3 grammars/Grammar.g4 -visitor
-mv grammars/*.py src/antlr_files/Proj_$proj/
+mv grammars/*.py src/antlr_files/
 mv grammars/*.interp src/antlr_files/
 mv grammars/*.tokens src/antlr_files/
