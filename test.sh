f14159177d5745cb2b98143c797661b7102189bd
CedricDeVijt <cedric.devijt@gmail.com>
2024-04-05 14:57:45 +0200

    added feature for choosing input folder

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 # Define the paths for rendering AST and symbol table, and compiling to LLVM and MIPS
 MAIN_SCRIPT="python -m src.main"
 OUTPUT_DIR="output"
+INPUT_DIR="tests"  # Default input directory
 
 # Create and activate Python 3 virtual environment
 python3 -m venv venv
@@ -20,16 +21,16 @@ process_file() {
     mips_output="$OUTPUT_DIR/${filename_without_extension}.mips"
 
     # Rendering the AST
-    $MAIN_SCRIPT --input "$input_file" --render_ast "$ast_output"
+    $MAIN_SCRIPT --input "$input_file" --render_ast_png "$ast_output"
 
-    # Rendering the symbol table
-    $MAIN_SCRIPT --input "$input_file" --render_symb "$symb_output"
-
-    # Compile to LLVM
-    $MAIN_SCRIPT --input "$input_file" --target_llvm "$llvm_output"
-
-    # Compile to MIPS
-    $MAIN_SCRIPT --input "$input_file" --target_mips "$mips_output"
+#    # Rendering the symbol table
+#    $MAIN_SCRIPT --input "$input_file" --render_symb "$symb_output"
+#
+#    # Compile to LLVM
+#    $MAIN_SCRIPT --input "$input_file" --target_llvm "$llvm_output"
+#
+#    # Compile to MIPS
+#    $MAIN_SCRIPT --input "$input_file" --target_mips "$mips_output"
 }
 
 # Recursive function to process files in a directory
@@ -47,5 +48,15 @@ process_directory() {
     done
 }
 
-# Start processing from the tests directory
-process_directory "tests"
+
+# Parse command-line options
+while getopts i: flag
+do
+    # shellcheck disable=SC2220
+    case "${flag}" in
+        i) INPUT_DIR=${OPTARG};;
+    esac
+done
+
+# Start processing from the input directory
+process_directory "$INPUT_DIR"
