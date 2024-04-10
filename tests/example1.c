b25d5347fb7d996c518b5d9d21df196c3ef0b9ab
thomas <thomas.urkens@gmail.com>
2024-04-10 19:00:06 +0200

    LLVM operations almost finished

--- tests/example1.c
+++ tests/example1.c
@@ -1,18 +1,7 @@
-typedef int testType;
 
-int main() {
-    int a = 5;
-    switch (a) {
-        case 4:
-            a = 5;
-        case 69:
-            a = 6;
-            break;
-        case 5:
-            a = 10;
-        default:
-            a = 20;
-            break;
-    }
-    printf("%d", a);
+
+int main () {
+    int a = 10;
+    int b = 5;
+    int c = a + b + 'd';
 }
