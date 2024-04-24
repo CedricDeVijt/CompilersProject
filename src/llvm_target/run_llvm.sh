4827f468315506f2ee8e64bef0d8224709b74f2d
Thomig <Thomas.Urkens@student.uantwerpen.be>
2024-04-24 15:43:45 +0200

    LLVM recent changes

--- src/llvm_target/run_llvm.sh
+++ src/llvm_target/run_llvm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if the input file exists
-if [ ! -f "output_file.ll" ]; then
-    echo "Error: output_file.ll does not exist."
+if [ ! -f "output.ll" ]; then
+    echo "Error: output.ll does not exist."
     exit 1
 fi
 
 # Compile the LLVM IR file to an executable
-clang output_file.ll -o testExe
+clang output.ll -o testExe
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
