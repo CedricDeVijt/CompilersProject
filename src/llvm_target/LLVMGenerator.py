b912bdcbeced55e5b45995a9e5596c6072611a47
Thomig <Thomas.Urkens@student.uantwerpen.be>
2024-04-25 16:43:47 +0200

    LLVMGenerator push

--- src/llvm_target/LLVMGenerator.py
+++ src/llvm_target/LLVMGenerator.py
@@ -202,24 +202,12 @@ class LLVMVisitor:
         self.printf_string += 1
         return self.builder.bitcast(string_global, ir.PointerType(ir.IntType(8)))
 
-    def visit_StringNode(self, node):
-        c_string_type = ir.ArrayType(ir.IntType(8), len(node.value) + 1)
-        string_global = ir.GlobalVariable(self.module, c_string_type, name=f'string_{self.printf_string}')
-        string_global.global_constant = True
-        string_global.initializer = ir.Constant(c_string_type, bytearray(node.value + '\00', 'utf8'))
-        return self.builder.bitcast(string_global, ir.PointerType(ir.IntType(8)))
-
     def visit_LogicalNotNode(self, node):
         # TODO FIX LOGICAL NOT INSTEAD OF BITWISE NOT - fuck llvm
         return self.builder.not_(self.visit(node.children[0]))
 
     def visit_BitwiseNotNode(self, node):
         return self.builder.not_(self.visit(node.children[0]))
-
-    def visit_PlusNode(self, node):
-        left = self.visit(node.children[0])
-        right = self.visit(node.children[1])
-        return self.builder.add(left, right)
 
     def visit_MinusNode(self, node):
         left = self.visit(node.children[0])
