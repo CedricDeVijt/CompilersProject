f066d5c3c59fd8d4018b0869a7646417fc8e113d
CedricDeVijt <cedric.devijt@gmail.com>
2024-04-30 15:51:46 +0200

    updates :)

--- src/tests/LLVM_tests/test_project_3.py
+++ src/tests/LLVM_tests/test_project_3.py
@@ -1,23 +1,23 @@
-# from src.tests.utils import llvm_output_compare
-#
-# root = "src/tests/test_input_files/extra_tests_with_print/project_3/"
-#
-#
-# def test_p3_comments():
-#     source_file = "p3_comments.c"
-#     llvm_output_compare(root, source_file)
-#
-#
-# def test_p3_example_input_file():
-#     source_file = "p3_example_input_file.c"
-#     llvm_output_compare(root, source_file)
-#
-#
-# def test_p3_printf():
-#     source_file = "p3_printf.c"
-#     llvm_output_compare(root, source_file)
-#
-#
+from src.tests.utils import llvm_output_compare
+
+root = "src/tests/test_input_files/extra_tests_with_print/project_3/"
+
+
+def test_p3_comments():
+    source_file = "p3_comments.c"
+    llvm_output_compare(root, source_file)
+
+
+def test_p3_example_input_file():
+    source_file = "p3_example_input_file.c"
+    llvm_output_compare(root, source_file)
+
+
+def test_p3_printf():
+    source_file = "p3_printf.c"
+    llvm_output_compare(root, source_file)
+
+
 # def test_p3_typedef_1():
 #     source_file = "p3_typedef_1.c"
 #     llvm_output_compare(root, source_file)
