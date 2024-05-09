93f53deb9410c440f0429cc73156739505387a73
CedricDeVijt <cedric.devijt@gmail.com>
2024-05-09 18:20:27 +0200

    MORE fixes

--- src/tests/test_input_files/custom_tests/project_5/p5_include.c
+++ src/tests/test_input_files/custom_tests/project_5/p5_include.c
@@ -1,4 +1,4 @@
-#include "src/tests/test_input_files/extra_tests_with_print/project_5/p5_include_header.h"
+#include "src/tests/test_input_files/custom_tests/project_5/p5_include_header.h"
 
 int main(){
     a= 10;
