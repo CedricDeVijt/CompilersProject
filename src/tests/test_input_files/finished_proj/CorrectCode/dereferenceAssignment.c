ecdc76efa47c06d05632264f48801c0649e36c46
CedricDeVijt <cedric.devijt@gmail.com>
2024-05-09 15:57:04 +0200

    test fix

--- src/tests/test_input_files/finished_proj/CorrectCode/dereferenceAssignment.c
+++ src/tests/test_input_files/finished_proj/CorrectCode/dereferenceAssignment.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-// This should print: 10, 10, 11, 11
+// This should print: 10, 10
 int main(){
 	int x = 0;
 	int* xp = &x;
 	*xp = 10;
-	printf("%d; ", x);
-	printf("%d\n", *xp);
-	(*xp)++;
 	printf("%d; ", x);
 	printf("%d\n", *xp);
 	return 0;
