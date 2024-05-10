7c22386eb0643a83b8b3ad8132a8935e99eabd5f
CedricDeVijt <cedric.devijt@gmail.com>
2024-05-10 15:16:42 +0200

    :zap: use of gcc to compile test files instead compiling to llvm ir and than running the llvm code.

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -14,9 +14,10 @@ def llvm_output_compare(root: str, input_file: str):
     llvm_generated_output = filename + "_generated_output.txt"
     llvm_test_output = filename + "_test_output.txt"
 
-    # compile to llvm and run with clang
-    os.system("clang -S -emit-llvm " + file_dir + source_file + " -o " + file_dir + llvm_generated_code)
-    os.system("lli " + file_dir + llvm_generated_code + " > " + file_dir + llvm_generated_output)
+    # compile the .c file using gcc
+    os.system("gcc " + file_dir + source_file + " -o " + file_dir + filename)
+    # run the executable and save the output to the text file
+    os.system(file_dir + filename + " > " + file_dir + llvm_generated_output)
 
     # compile to llvm and run with our compiler
     compile_llvm(input_file=file_dir + source_file, visitor=visitor, output_file=file_dir + llvm_test_code,
