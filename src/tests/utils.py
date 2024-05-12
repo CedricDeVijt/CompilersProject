2aec17aa5fad190bf1f6aba80dae57440ba2792b
CedricDeVijt <cedric.devijt@gmail.com>
2024-05-12 17:31:35 +0200

    WORKS ON MY MACHINE

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -104,7 +104,7 @@ def mips_output_compare(root: str, input_file: str):
     print(mips_output_text)
 
     # assert that the outputs are the same
-    assert gcc_output_text == mips_output_text
+    assert gcc_output_text.__str__() == mips_output_text.__str__()
 
 
 def mips_output_compare_with_expected_output(root: str, input_file: str, expected_output: str):
