5dc81a42c1719714de13ae93d0a68c68381ef084
CedricDeVijt <cedric.devijt@gmail.com>
2024-05-12 15:34:52 +0200

    added tests for mips

---
+++ src/tests/MIPS_tests/test_project_7.py
@@ -0,0 +1,23 @@
+# from src.tests.utils import llvm_output_compare, llvm_output_compare_with_expected_output
+#
+# root = "src/tests/test_input_files/custom_tests/project_7/"
+#
+#
+# def test_p7_struct():
+#     source_file = "p7_struct.c"
+#     llvm_output_compare(root, source_file)
+#
+#
+# def test_p7_struct_2():
+#     source_file = "p7_struct_2.c"
+#     llvm_output_compare(root, source_file)
+#
+#
+# def test_p7_struct_assignment():
+#     source_file = "p7_struct_assignment.c"
+#     llvm_output_compare(root, source_file)
+#
+#
+# def test_p7_struct_assignment2():
+#     source_file = "p7_struct_assignment2.c"
+#     llvm_output_compare(root, source_file)
