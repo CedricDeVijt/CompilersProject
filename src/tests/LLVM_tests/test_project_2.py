e79458add8639fabaff43933c435dd9550a00fa6
CedricDeVijt <cedric.devijt@gmail.com>
2024-05-12 18:29:08 +0200

    added pytests

--- src/tests/LLVM_tests/test_project_2.py
+++ src/tests/LLVM_tests/test_project_2.py
@@ -3,6 +3,26 @@ from src.tests.utils import llvm_output_compare
 root = "src/tests/test_input_files/custom_tests/project_2/"
 
 
+def test_p2_bin_op_1():
+    source_file = "p2_bin_op_1.c"
+    llvm_output_compare(root, source_file)
+
+
+def test_p2_bin_op_2():
+    source_file = "p2_bin_op_2.c"
+    llvm_output_compare(root, source_file)
+
+
+def test_p2_bitw_op():
+    source_file = "p2_bitw_op.c"
+    llvm_output_compare(root, source_file)
+
+
+def test_p2_comp_op():
+    source_file = "p2_comp_op.c"
+    llvm_output_compare(root, source_file)
+
+
 def test_p2_const():
     source_file = "p2_const.c"
     llvm_output_compare(root, source_file)
@@ -33,6 +53,21 @@ def test_p2_literals():
     llvm_output_compare(root, source_file)
 
 
+def test_p2_parenthesis():
+    source_file = "p2_parenthesis.c"
+    llvm_output_compare(root, source_file)
+
+
 def test_p2_pointers():
     source_file = "p2_pointers.c"
+    llvm_output_compare(root, source_file)
+
+
+def test_p2_shift_op():
+    source_file = "p2_shift_op.c"
+    llvm_output_compare(root, source_file)
+
+
+def test_p2_unary_op():
+    source_file = "p2_unary_op.c"
     llvm_output_compare(root, source_file)
