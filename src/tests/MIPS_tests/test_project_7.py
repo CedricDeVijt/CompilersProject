bc2e271be56e3bc4d66e21f350c88733a5bf894c
Thomig <Thomas.Urkens@student.uantwerpen.be>
2024-05-17 17:18:19 +0200

    MIPS: test script updated

--- src/tests/MIPS_tests/test_project_7.py
+++ src/tests/MIPS_tests/test_project_7.py
@@ -25,3 +25,11 @@ def test_p7_struct_assignment2():
 def test_p7_struct_definition():
     source_file = "test_p7_struct_definition.c"
     mips_output_compare(root, source_file)
+
+def test_p7_struct_operations():
+    source_file = "test_p7_struct_operations.c"
+    mips_output_compare(root, source_file)
+
+def test_p7_struct_var_assignment():
+    source_file = "test_p7_struct_var_assignment.c"
+    mips_output_compare(root, source_file)
