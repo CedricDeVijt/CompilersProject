94a78a87c08e4225193718ea61eb7d113afc20c3
robeb <robbe.stuer@gmail.com>
2024-05-18 12:33:42 +0200

    Added return to tests

--- src/tests/test_input_files/custom_tests/project_2/p2_const.c
+++ src/tests/test_input_files/custom_tests/project_2/p2_const.c
@@ -2,4 +2,5 @@ int main() {
     const int a = 5;
     int b = 10;
     const int* p = &b;
+    return 0;
 }
--- src/tests/test_input_files/custom_tests/project_2/p2_const_casting.c
+++ src/tests/test_input_files/custom_tests/project_2/p2_const_casting.c
@@ -3,4 +3,5 @@ int main() {
    const float* f_ptr = &f;
    float* non_const_f_ptr = f_ptr;
    *non_const_f_ptr = 3.1492;
+   return 0;
 }
