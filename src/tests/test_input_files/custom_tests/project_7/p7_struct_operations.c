ba731f6da380e26a2a476fcfdb2586a6fb022e0d
Thomig <Thomas.Urkens@student.uantwerpen.be>
2024-05-29 22:04:56 +0200

    updated test

--- src/tests/test_input_files/custom_tests/project_7/p7_struct_operations.c
+++ src/tests/test_input_files/custom_tests/project_7/p7_struct_operations.c
@@ -10,10 +10,11 @@ struct test{
 int main(){
     struct test t;
     t.a = 10;
-    t.a++;  // not sure if we want to add this
+    t.a++;
+    ++t.a;
     t.b = 20 + 5;
     t.c = 23.6;
-    t.c--;
+    t.b--;
     t.d = 'h';
     t.d = 'i';
     printf("%d, ,%d, %f, %c\n", t.a, t.b, t.c, t.d);
